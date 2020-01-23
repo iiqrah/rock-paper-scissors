af9f4808b77458af8c0ce25032b0d5b951fc83df
inadeem <inadeem@cyprotex.com>
2020-01-23 16:35:06 +0000

    Add JS script and cached the DOM

---
+++ app.js
@@ -0,0 +1,11 @@
+let userScore = 0;
+let compScore = 0;
+
+const userScore_span = document.getElementById('user-score');
+const compScore_span = document.getElementById('comp-score');
+
+const resultMessage_p = document.getElementsByClassName('result-message');
+
+const rock_div = document.getElementById('rock');
+const paper_div = document.getElementById('paper');
+const scissors_div = document.getElementById('scissors');
