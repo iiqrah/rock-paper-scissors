f876444b0e643335be7265c6f36555f796707c7d
inadeem <inadeem@cyprotex.com>
2020-01-23 16:54:32 +0000

    Add get user and computer choice functionality

--- app.js
+++ app.js
@@ -9,3 +9,40 @@ const resultMessage_p = document.getElementsByClassName('result-message');
 const rock_div = document.getElementById('rock');
 const paper_div = document.getElementById('paper');
 const scissors_div = document.getElementById('scissors');
+
+function getCompChoice() {
+
+  let compChoiceArr = ['rock', 'paper', 'scissors'];
+  let randomNumber = Math.floor(Math.random()*3);
+  return compChoiceArr[randomNumber];
+
+}
+
+function game(userChoice) {
+
+  compChoice = getCompChoice();
+
+  console.log("You pressed "+ userChoice);
+  console.log("Comp pressed "+ compChoice);
+
+
+}
+
+
+function getUserChoice() {
+
+  rock_div.addEventListener('click', function(){
+    game("rock");
+  })
+
+  paper_div.addEventListener('click', function(){
+    game("paper");
+  })
+
+  scissors_div.addEventListener('click', function(){
+    game("scissors");
+  })
+
+}
+
+getUserChoice();
